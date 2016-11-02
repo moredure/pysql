e9721b38744073ca8e1875aeaf993ff8a0fe1c64
m.faraponov <mikefaraponov@gmail.com>
2016-11-02 18:51:06 +0200

    Added templates and removed CI email notifications

--- pysql/schema.sql
+++ pysql/schema.sql
@@ -3,11 +3,13 @@ drop table if exists flags;
 
 create table users (
   id integer primary key autoincrement,
-  name text not null,
+  login text not null,
+  username text not null,
+  password_digest text not null,
   age int not null
 );
 
-create table flags (
+create table secrets (
   id integer primary key autoincrement,
   secret text not null
 );
